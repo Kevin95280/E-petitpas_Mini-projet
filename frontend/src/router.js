3a9811082f5ca22afe8cdb6817fe4b5724a4110e
Kevin95280 <coelhokev@ymail.com>
2025-10-19 11:39:02 +0200

    Docker config

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-import Profil from "./pages//Profil";
+import Profil from "./pages/Profil";
 
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/profil" element={<Profil />} />
       </Routes>
     </Router>
